8ad1113bef1077f35d287d7031ef8cf603f7cd93
GodsplanElot <elotgodsplan2@gmail.com>
2025-11-08 05:01:29 +0100

    modify settings

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
 ]
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

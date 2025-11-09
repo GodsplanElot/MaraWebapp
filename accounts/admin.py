d8c242d3c260bc686f64f0c4893bb810acfdac4a
GodsplanElot <elotgodsplan2@gmail.com>
2025-11-09 23:53:57 +0100

    modify admin for account app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('balance', 'is_kyc_verified', 'wallet_address')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
